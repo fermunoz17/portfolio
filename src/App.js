7f8e160f7d62d210f7df105b9b090c13baed8699
fermunoz17 <fermunozley@gmail.com>
2026-04-15 22:36:30 -0600

    Updated Bio

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Achievements from './components/Achievements';
 import Projects from './components/Projects'
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path="/projects" element={<Projects />} />
 
       </Routes>
+      <Footer />
     </Router>
 
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>Built with React, React Bootstrap, and Framer Motion</p>
+        </footer>
+    );
+}
+
+export default Footer;
